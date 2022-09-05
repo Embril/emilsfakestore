3930037e248359216615a859670dec8156be5c8f
Emil <emil.sjursen@metieroec.no>
2022-09-05 09:42:13 +0200

    Fixed cors error

--- src/Component/Login.jsx
+++ src/Component/Login.jsx
@@ -19,30 +19,27 @@ const Login = () => {
     try {
       const response = await fetch('https://fakestoreapi.com/auth/login', {
         method: 'POST',
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+          'Content-Type': 'application/json',
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
         body: JSON.stringify({
           username: userName,
           password: password,
         }),
       })
-      if (userName == 'emil' && password == 'emil')
-        dispatch(authenticateSuccess('Emil'))
-      else dispatch(authenticateFailed('Wrong Password'))
+      if (response) dispatch(authenticateSuccess(userName))
+      else dispatch(authenticateFailed('Wrong Username or Password'))
     } catch (error) {
       console.warn(error)
-      dispatch(authenticateFailed('Wrong Password'))
+      dispatch(authenticateFailed('Wrong Username or Password'))
     }
   }
-
-  // function authenticate() {
-  //   let jqxhr = jQuery
-  //     .post('https://fakestoreapi.com/auth/login', {
-  //       userName: userName,
-  //       password: password,
-  //     })
-  //     .done(function (jqxhr) {
-  //       console.log(jqxhr)
-  //     })
-  // }
 
   return (
     <div>
